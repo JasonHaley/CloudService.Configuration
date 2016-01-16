e4198950467ff8359d242603e6071aa70ee87147
Jason Haley <jason.haley@finomial.com>
2016-01-16 15:44:51 -0500

    Initial commit

---
+++ CloudService.Configuration/src/CloudService.Configuration/CloudServiceConfigurationProvider.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.WindowsAzure.ServiceRuntime;
+
+namespace CloudService.Configuration
+{
+    public class CloudServiceConfigurationProvider : ConfigurationProvider
+    {
+        public CloudServiceConfigurationProvider(IEnumerable<string> settingNames)
+        {
+            if (settingNames == null)
+            {
+                throw new ArgumentNullException(nameof(settingNames));
+            }
+
+            SettingNames = settingNames;
+        }
+
+        protected IEnumerable<string> SettingNames { get; private set; }
+
+        public override void Load()
+        {
+            var data = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+
+            foreach (var settingName in SettingNames)
+            {
+                var value = RoleEnvironment.GetConfigurationSettingValue(settingName);
+                if (!string.IsNullOrEmpty(value))
+                {
+                    data.Add(settingName, value);
+                }
+            }
+
+            Data = data;
+        }
+    }
+}
---
+++ CloudService.Configuration/src/CloudService.Configuration/CloudServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CloudService.Configuration
+{
+    public static class CloudServiceExtensions
+    {
+        public static IConfigurationBuilder AddCloudServiceConfig(this IConfigurationBuilder builder, IEnumerable<string> settingNames)
+        {
+            builder.Add(new CloudServiceConfigurationProvider(settingNames));
+            return builder;
+        }
+    }
+}
